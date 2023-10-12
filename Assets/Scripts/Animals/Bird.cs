41db409601f07729c1f12773551823acded44478
Martyna Cichosz <96289919+zielonoludek@users.noreply.github.com>
2023-10-12 15:41:31 +0200

    Bird Animation (#9)
    Co-authored-by: Tvejzer <art2q5@gmail.com>

--- Assets/Scripts/Animals/Bird.cs
+++ Assets/Scripts/Animals/Bird.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class Bird : Animal
 {
     private Vector3 target;
-    [SerializeField] private int targetNum;
-    [SerializeField] private GameObject targetList;
+    private int targetNum;
+    private GameObject targetList;
+    [SerializeField] Animator animator;
+
 
     private void Start()
     {
@@ -18,6 +20,7 @@ public class Bird : Animal
     {
         transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
         if (transform.position == target) RandomiseTarget();
+        animator.SetFloat("Speed", speed);
     }
     private void RandomiseTarget()
     {
