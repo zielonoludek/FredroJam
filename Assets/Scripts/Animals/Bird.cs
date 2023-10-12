9584e79b134cc99444cd397fc42c1dacfb36ba89
Tvejzer <112761214+Tvejzer@users.noreply.github.com>
2023-10-12 19:49:53 +0200

    Animals (#12)
    * Animations
    
    * More Animations and player
    
    * New sprites
    
    * Door sprite

--- Assets/Scripts/Animals/Bird.cs
+++ Assets/Scripts/Animals/Bird.cs
@@ -5,8 +5,6 @@ public class Bird : Animal
     private Vector3 target;
     private int targetNum;
     private GameObject targetList;
-    [SerializeField] Animator animator;
-
 
     private void Start()
     {
@@ -20,7 +18,6 @@ public class Bird : Animal
     {
         transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
         if (transform.position == target) RandomiseTarget();
-        animator.SetFloat("Speed", speed);
     }
     private void RandomiseTarget()
     {
